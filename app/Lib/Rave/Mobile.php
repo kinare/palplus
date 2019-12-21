062538e9655a4770da7b90f99e525864d6c60ea4
Michael <michael.kamau@kinetics.co.ke>
2019-12-21 18:38:39 +0300

    Mobile rave bbugfix

--- app/Lib/Rave/Mobile.php
+++ app/Lib/Rave/Mobile.php
@@ -12,24 +12,6 @@ class Mobile extends Rave
 {
     public function transact(GatewayTransaction $transaction){
         $data = json_decode($transaction->payload, true);
-        return $this->initiate($data);
-    }
-
-    public function initiate($data)
-    {
-        /* encrypt request data */
-        $details = $this->encrypt($data);
-
-        /* prepare postdata */
-        $postdata = [
-            'PBFPubKey' => Config::getConfig('RAVE_PUBLIC_KEY'),
-            'client' => $details,
-            'alg' => '3DES-24'
-        ];
-
-        /* Execute request */
-        $res = $this->execute($postdata, 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/charge');
-
-        return $res;
+        return $this->initiate($data, 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/charge');
     }
 }
