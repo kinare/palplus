aa6602ec2ee6a183ecf55f93f06f0de22db849b2
macs-code <omambiadauglous@gmail.com>
2020-05-17 10:19:41 +0300

    [fix] add to wallet and remove member from the group

--- app/Notification.php
+++ app/Notification.php
@@ -14,9 +14,18 @@ class Notification extends BaseModel
       'status',
       'notification_types_id',
     ];
-
+	/**
+	 * undocumented function summary
+	 *
+	 * Undocumented function long description
+	 *
+	 * @param Type $Array notice
+	 * @return type
+	 * values  => subject, message, payload, notification_types_id, user_id
+	 **/
     public static function make(array $notice) : self
     {
+		// notic
         $self = new self();
         $self->fill($notice);
         $self->created_by = Auth::user()->id;
@@ -28,5 +37,5 @@ class Notification extends BaseModel
         $this->status = 'inactive';
         $this->modified_by = Auth::user()->id;
         $this->save();
-    }
+	}
 }
