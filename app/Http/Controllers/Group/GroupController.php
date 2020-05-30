f2a6b297368fa25dcb350f42e5a3f845e942c5c8
macs-code <omambiadauglous@gmail.com>
2020-05-30 12:06:15 +0300

    [test] create group

--- app/Http/Controllers/Group/GroupController.php
+++ app/Http/Controllers/Group/GroupController.php
@@ -137,7 +137,8 @@ class GroupController extends BaseController
             //     Storage::disk('avatars')->put("groups/".$model->code.'/avatar.png', (string) $avatar);
 			// 	$model->avatar =  'avatar.png';
 			// }
-			// $avatar = Avatar::create($model->name)->getImageObject()->encode('png');
+			$avatar = Avatar::create($model->name)->getImageObject()->encode('png');
+			dd($avatar);
 			Storage::disk('avatars')->put("groups/".$model->code.'/avatar.png', (string) $avatar);
 			$model->avatar =  'avatar.png';
 			dd($model);
