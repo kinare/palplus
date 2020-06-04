2708f117ae0882e2a235a47ced0fd25ee8257394
macs-code <omambiadauglous@gmail.com>
2020-06-04 19:54:12 +0300

    [chore] handle url parameters

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
 
 Route::get('/response', function (Request $request) {
 	$body = $request->route()->parameter('response');
-	dd($body);
+	// dd($body);
 	$response = json_decode($body);
 
 	return view('response', ['status' => $response->status]);
