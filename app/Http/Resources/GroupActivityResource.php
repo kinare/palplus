89dc52b7f6ec1f2b12857ebf1dd4a3553bb5b773
macs-code <omambiadauglous@gmail.com>
2020-06-28 21:09:24 +0300

    created at date format activities

--- app/Http/Resources/GroupActivityResource.php
+++ app/Http/Resources/GroupActivityResource.php
@@ -20,6 +20,7 @@ class GroupActivityResource extends JsonResource
         $data['is_member'] = $this->hasJoined($this->group_id) ? true : false;
         $data['currency'] = Wallet::group($this->group_id)->walletCurrency();
         $data['cut_off_date_readable'] = Carbon::parse($data['cut_off_date'])->toFormattedDateString();
+        $data['created_at'] = Carbon::parse($data['created_at'])->toFormattedDateString();
         $data['start_date_readable'] = Carbon::parse($data['start_date'])->toFormattedDateString();
         $data['end_date_readable'] = Carbon::parse($data['end_date'])->toFormattedDateString();
         return $data;
